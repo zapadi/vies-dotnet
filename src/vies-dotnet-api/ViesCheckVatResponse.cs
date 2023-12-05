f4d7cdf11ec1a4ca5ca53036210eca23417f511d
Adrian Popescu <adi.popescu@gmail.com>
2023-12-05 14:15:41 +0200

    [ViesCheckVatResponse] Implement IEquatable & implicit operator

--- src/vies-dotnet-api/ViesCheckVatResponse.cs
+++ src/vies-dotnet-api/ViesCheckVatResponse.cs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2017-2022 Adrian Popescu.
+   Copyright 2017-2023 Adrian Popescu.
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
@@ -15,23 +15,65 @@ using System;
 
 namespace Padi.Vies;
 
-    public struct ViesCheckVatResponse
+public struct ViesCheckVatResponse: IEquatable<ViesCheckVatResponse>
+{
+    public ViesCheckVatResponse(string countryCode, string vatNumber, DateTimeOffset requestDate, string name = null, string address = null, bool isValid = false) : this()
     {
-        public ViesCheckVatResponse(string countryCode, string vatNumber, DateTimeOffset requestDate, string name = null, string address = null, bool isValid = false) : this()
+        this.CountryCode = countryCode;
+        this.VatNumber = vatNumber;
+        this.RequestDate = requestDate;
+        this.Name = name;
+        this.Address = address;
+        this.IsValid = isValid;
+    }
+
+    public string CountryCode { get; internal set; }
+    public string VatNumber { get; internal set; }
+    public DateTimeOffset RequestDate { get; internal set;}
+    public string Name { get; internal set;}
+    public string Address { get; internal set;}
+    public bool IsValid { get; internal set;}
+
+    public readonly bool Equals(ViesCheckVatResponse other)
+    {
+        return
+            string.Equals(this.CountryCode, other.CountryCode, StringComparison.OrdinalIgnoreCase) &&
+            string.Equals(this.VatNumber, other.VatNumber, StringComparison.OrdinalIgnoreCase) &&
+            string.Equals(this.Name, other.Name, StringComparison.OrdinalIgnoreCase) &&
+            string.Equals(this.Address, other.Address, StringComparison.OrdinalIgnoreCase) && this.RequestDate.Equals(other.RequestDate) && this.IsValid == other.IsValid;
+    }
+
+    public override readonly bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj))
         {
-            CountryCode = countryCode;
-            VatNumber = vatNumber;
-            RequestDate = requestDate;
-            Name = name;
-            Address = address;
-            IsValid = isValid;
+            return false;
         }
 
-        public string CountryCode { get; internal set; }
-        public string VatNumber { get; internal set; }
-        public DateTimeOffset RequestDate { get; internal set;}
-        public string Name { get; internal set;}
-        public string Address { get; internal set;}
-        public bool IsValid { get; internal set;}
+        return obj is ViesCheckVatResponse response && this.Equals(response);
     }
-}
+
+    public override readonly int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = (this.CountryCode != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.CountryCode) : 0);
+            hashCode = (hashCode * 397) ^ (this.VatNumber != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.VatNumber) : 0);
+            hashCode = (hashCode * 397) ^ this.RequestDate.GetHashCode();
+            hashCode = (hashCode * 397) ^ (this.Name != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.Name) : 0);
+            hashCode = (hashCode * 397) ^ (this.Address != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.Address) : 0);
+            hashCode = (hashCode * 397) ^ this.IsValid.GetHashCode();
+            return hashCode;
+        }
+    }
+
+    public static bool operator ==(ViesCheckVatResponse left, ViesCheckVatResponse right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ViesCheckVatResponse left, ViesCheckVatResponse right)
+    {
+        return !(left == right);
+    }
+}
