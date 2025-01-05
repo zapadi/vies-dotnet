3fec0d4e9ba7c12cec694378e622fcec7160f0fc
Adrian Popescu <adi.popescu@gmail.com>
2025-01-05 10:42:08 +0200

    [ViesCheckVatResponse] Change from struct to record struct

--- src/vies-dotnet-api/ViesCheckVatResponse.cs
+++ src/vies-dotnet-api/ViesCheckVatResponse.cs
@@ -15,65 +15,18 @@ using System;
 
 namespace Padi.Vies;
 
-public struct ViesCheckVatResponse: IEquatable<ViesCheckVatResponse>
+public record struct ViesCheckVatResponse(
+    string CountryCode,
+    string VatNumber,
+    DateTimeOffset RequestDate,
+    string Name = null,
+    string Address = null,
+    bool IsValid = false)
 {
-    public ViesCheckVatResponse(string countryCode, string vatNumber, DateTimeOffset requestDate, string name = null, string address = null, bool isValid = false) : this()
-    {
-        this.CountryCode = countryCode;
-        this.VatNumber = vatNumber;
-        this.RequestDate = requestDate;
-        this.Name = name;
-        this.Address = address;
-        this.IsValid = isValid;
-    }
-
-    public string CountryCode { get; internal set; }
-    public string VatNumber { get; internal set; }
-    public DateTimeOffset RequestDate { get; internal set;}
-    public string Name { get; internal set;}
-    public string Address { get; internal set;}
-    public bool IsValid { get; internal set;}
-
-    public readonly bool Equals(ViesCheckVatResponse other)
-    {
-        return
-            string.Equals(this.CountryCode, other.CountryCode, StringComparison.OrdinalIgnoreCase) &&
-            string.Equals(this.VatNumber, other.VatNumber, StringComparison.OrdinalIgnoreCase) &&
-            string.Equals(this.Name, other.Name, StringComparison.OrdinalIgnoreCase) &&
-            string.Equals(this.Address, other.Address, StringComparison.OrdinalIgnoreCase) && this.RequestDate.Equals(other.RequestDate) && this.IsValid == other.IsValid;
-    }
-
-    public readonly override bool Equals(object obj)
-    {
-        if (ReferenceEquals(null, obj))
-        {
-            return false;
-        }
-
-        return obj is ViesCheckVatResponse response && this.Equals(response);
-    }
-
-    public readonly override int GetHashCode()
-    {
-        unchecked
-        {
-            var hashCode = (this.CountryCode != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.CountryCode) : 0);
-            hashCode = (hashCode * 397) ^ (this.VatNumber != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.VatNumber) : 0);
-            hashCode = (hashCode * 397) ^ this.RequestDate.GetHashCode();
-            hashCode = (hashCode * 397) ^ (this.Name != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.Name) : 0);
-            hashCode = (hashCode * 397) ^ (this.Address != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(this.Address) : 0);
-            hashCode = (hashCode * 397) ^ this.IsValid.GetHashCode();
-            return hashCode;
-        }
-    }
-
-    public static bool operator ==(ViesCheckVatResponse left, ViesCheckVatResponse right)
-    {
-        return left.Equals(right);
-    }
-
-    public static bool operator !=(ViesCheckVatResponse left, ViesCheckVatResponse right)
-    {
-        return !(left == right);
-    }
+    public string CountryCode { get; internal set; } = CountryCode;
+    public string VatNumber { get; internal set; } = VatNumber;
+    public DateTimeOffset RequestDate { get; internal set;} = RequestDate;
+    public string Name { get; internal set;} = Name;
+    public string Address { get; internal set;} = Address;
+    public bool IsValid { get; internal set;} = IsValid;
 }
