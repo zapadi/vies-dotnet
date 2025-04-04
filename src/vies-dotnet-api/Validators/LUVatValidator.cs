70c7150d54153faf2161e1a1948245b062a286af
Padi <adi.popescu@gmail.com>
2025-04-04 18:56:50 +0300

    Update Copyright (#33)

--- src/vies-dotnet-api/Validators/LUVatValidator.cs
+++ src/vies-dotnet-api/Validators/LUVatValidator.cs
@@ -1,6 +1,6 @@
 using System;
 /*
-   Copyright 2017-2024 Adrian Popescu.
+   Copyright 2017-2025 Adrian Popescu.
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
