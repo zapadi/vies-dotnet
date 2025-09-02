31e2d3bb081ccdd54308a8b250b3d11c45ee1557
Adrian Popescu <adi.popescu@gmail.com>
2025-09-02 22:14:46 +0300

    [Refactor] Exceptions replace magic strings with constants

--- src/vies-dotnet-api/Errors/ViesErrorCodes.cs
+++ src/vies-dotnet-api/Errors/ViesErrorCodes.cs
@@ -1,4 +1,4 @@
-/*
+﻿/*
    Copyright 2017-2025 Adrian Popescu.
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
