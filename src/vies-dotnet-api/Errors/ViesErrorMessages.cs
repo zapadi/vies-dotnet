1faa0a8b16e0a4b91b5d4f6f3002a1177fb1c805
Adrian Popescu <adi.popescu+gitkraken@gmail.com>
2026-08-16 09:50:14 +0300

    feat: Expose public error code, type and message constants

---
+++ src/vies-dotnet-api/Errors/ViesErrorMessages.cs
@@ -0,0 +1,76 @@
+/*
+   Copyright 2017-2026 Adrian Popescu.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+namespace Padi.Vies.Errors;
+
+/// <summary>
+/// Provides the default messages associated with each <see cref="ViesErrorCode"/> value.
+/// </summary>
+public static class ViesErrorMessages
+{
+    /// <summary>
+    /// Returns the default message for the given error code.
+    /// </summary>
+    /// <param name="errorCode">One of the <see cref="ViesErrorCode"/> constants.</param>
+    /// <returns>The default message or null when the error code is unknown.</returns>
+    public static string GetDefaultMessage(string errorCode)
+    {
+        return errorCode switch
+        {
+            ViesErrorCode.VatInactive => ViesErrorCodes.InvalidRequestError.VatInactive.Message,
+            ViesErrorCode.VatNotFound => ViesErrorCodes.InvalidRequestError.VatNotFound.Message,
+            ViesErrorCode.ServiceUnavailable => ViesErrorCodes.ServiceError.ServiceUnavailable.Message,
+            ViesErrorCode.NetworkError => ViesErrorCodes.ServiceError.NetworkError.Message,
+            ViesErrorCode.Timeout => ViesErrorCodes.ServiceError.Timeout.Message,
+            ViesErrorCode.InvalidResponse => ViesErrorCodes.ServiceError.InvalidResponse.Message,
+            ViesErrorCode.RateLimitExceeded => ViesErrorCodes.ServiceError.RateLimitExceeded.Message,
+            ViesErrorCode.InvalidInput => ViesErrorCodes.InputError.InvalidInput.Message,
+            ViesErrorCode.InvalidVatFormat => ViesErrorCodes.ValidationError.InvalidVatFormat.Message,
+            ViesErrorCode.InvalidVatChecksumDigit => ViesErrorCodes.ValidationError.InvalidVatChecksumDigit.Message,
+            ViesErrorCode.InvalidCountryCode => ViesErrorCodes.ValidationError.InvalidCountryCode.Message,
+            ViesErrorCode.VatNumberTooLong => ViesErrorCodes.ValidationError.VatNumberTooLong.Message,
+            ViesErrorCode.RegionUnsupported => ViesErrorCodes.UnsupportedRegionError.RegionUnsupported.Message,
+            ViesErrorCode.UnexpectedError => ViesErrorCodes.ApiError.UnexpectedError.Message,
+            ViesErrorCode.DeserializationFailed => ViesErrorCodes.DeserializationError.Failed.Message,
+            _ => null,
+        };
+    }
+
+    /// <summary>
+    /// Returns the default user-facing message for the given error code.
+    /// </summary>
+    /// <param name="errorCode">One of the <see cref="ViesErrorCode"/> constants.</param>
+    /// <returns>The default user message or null when the error code is unknown.</returns>
+    public static string GetDefaultUserMessage(string errorCode)
+    {
+        return errorCode switch
+        {
+            ViesErrorCode.VatInactive => ViesErrorCodes.InvalidRequestError.VatInactive.UserMessage,
+            ViesErrorCode.VatNotFound => ViesErrorCodes.InvalidRequestError.VatNotFound.UserMessage,
+            ViesErrorCode.ServiceUnavailable => ViesErrorCodes.ServiceError.ServiceUnavailable.UserMessage,
+            ViesErrorCode.NetworkError => ViesErrorCodes.ServiceError.NetworkError.UserMessage,
+            ViesErrorCode.Timeout => ViesErrorCodes.ServiceError.Timeout.UserMessage,
+            ViesErrorCode.InvalidResponse => ViesErrorCodes.ServiceError.InvalidResponse.UserMessage,
+            ViesErrorCode.RateLimitExceeded => ViesErrorCodes.ServiceError.RateLimitExceeded.UserMessage,
+            ViesErrorCode.InvalidInput => ViesErrorCodes.InputError.InvalidInput.Message,
+            ViesErrorCode.InvalidVatFormat => ViesErrorCodes.ValidationError.InvalidVatFormat.UserMessage,
+            ViesErrorCode.InvalidVatChecksumDigit => ViesErrorCodes.ValidationError.InvalidVatChecksumDigit.UserMessage,
+            ViesErrorCode.InvalidCountryCode => ViesErrorCodes.ValidationError.InvalidCountryCode.UserMessage,
+            ViesErrorCode.VatNumberTooLong => ViesErrorCodes.ValidationError.VatNumberTooLong.UserMessage,
+            ViesErrorCode.RegionUnsupported => ViesErrorCodes.UnsupportedRegionError.RegionUnsupported.Message,
+            ViesErrorCode.UnexpectedError => ViesErrorCodes.ApiError.UnexpectedError.UserMessage,
+            ViesErrorCode.DeserializationFailed => ViesErrorCodes.DeserializationError.Failed.UserMessage,
+            _ => null,
+        };
+    }
+}
---
+++ tests/vies-dotnet-api-test/ViesErrorMessagesTests.cs
@@ -0,0 +1,79 @@
+/*
+   Copyright 2017-2026 Adrian Popescu.
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+       http://www.apache.org/licenses/LICENSE-2.0
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Padi.Vies.Errors;
+using Xunit;
+
+namespace Padi.Vies.Test;
+
+public sealed class ViesErrorMessagesTests
+{
+    private static IEnumerable<string> GetAllErrorCodes()
+    {
+        return typeof(ViesErrorCode)
+            .GetFields(BindingFlags.Public | BindingFlags.Static)
+            .Where(static field => field.IsLiteral && field.FieldType == typeof(string))
+            .Select(static field => (string)field.GetRawConstantValue());
+    }
+
+    public static TheoryData<string> ErrorCodes()
+    {
+        var data = new TheoryData<string>();
+        foreach (var code in GetAllErrorCodes())
+        {
+            data.Add(code);
+        }
+
+        return data;
+    }
+
+    [Theory]
+    [MemberData(nameof(ErrorCodes))]
+    public void Should_Return_Default_Message_For_Known_Code(string errorCode)
+    {
+        var message = ViesErrorMessages.GetDefaultMessage(errorCode);
+
+        Assert.False(string.IsNullOrEmpty(message));
+    }
+
+    [Theory]
+    [MemberData(nameof(ErrorCodes))]
+    public void Should_Return_Default_User_Message_For_Known_Code(string errorCode)
+    {
+        var userMessage = ViesErrorMessages.GetDefaultUserMessage(errorCode);
+
+        Assert.False(string.IsNullOrEmpty(userMessage));
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("no-such-error-code")]
+    public void Should_Return_Null_For_Unknown_Code(string errorCode)
+    {
+        Assert.Null(ViesErrorMessages.GetDefaultMessage(errorCode));
+        Assert.Null(ViesErrorMessages.GetDefaultUserMessage(errorCode));
+    }
+
+    [Fact]
+    public void Should_Have_Unique_Error_Codes()
+    {
+        var codes = GetAllErrorCodes().ToList();
+
+        Assert.NotEmpty(codes);
+        Assert.Equal(codes.Count, codes.Distinct(System.StringComparer.Ordinal).Count());
+    }
+}
