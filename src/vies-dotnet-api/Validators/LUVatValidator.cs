43eef97ac2d7a4b9d6c6c43ba81540fa34ff85e5
Adrian Popescu <adi.popescu+gitkraken@gmail.com>
2026-08-16 09:50:14 +0300

    chore: Update copyright header

--- src/vies-dotnet-api/Validators/LUVatValidator.cs
+++ src/vies-dotnet-api/Validators/LUVatValidator.cs
@@ -3,7 +3,7 @@ using Padi.Vies.Errors;
 using Padi.Vies.Internal.Extensions;
 
 /*
-   Copyright 2017-2025 Adrian Popescu.
+   Copyright 2017-2026 Adrian Popescu.
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
